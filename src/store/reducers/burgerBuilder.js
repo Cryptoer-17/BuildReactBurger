ddbae93690f264191f5cf0573ade13a93a026ceb
Andrea17 <andrew.iacoo@gmail.com>
2020-03-06 11:59:35 +0100

    funziona correttamente l'autenticazione

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from '../utility';
 const initialState = {
     ingredients: null,
     totalPrice: 4,
-    error: false
+    error: false,
+    building: false
 };
 
 const INGREDIENT_PRICES = {
@@ -17,11 +18,11 @@ const INGREDIENT_PRICES = {
 const addIngredient = ( state, action ) => {
     const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
     const updatedIngredients = updateObject( state.ingredients, updatedIngredient );
-    console.log(updatedIngredients);
     const updatedState = {
         
         ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building : true
     }
     return updateObject( state, updatedState );
 };
@@ -45,7 +46,8 @@ const setIngredients = (state, action) => {
             meat: action.ingredients.meat
         },
         totalPrice: 4,
-        error: false
+        error: false,
+        building:false
     } );
 };
 
